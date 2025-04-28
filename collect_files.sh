02545d36cf3edb0d02126ed197b7fdbc1689bdee
kl1ment <122699425+Saltytie@users.noreply.github.com>
2025-04-28 23:19:17 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,67 +1,75 @@
 #!/bin/bash
 
-
-max_depth=0
+max_depth=""
 input_dir=""
 output_dir=""
 
-if [[ "$1" == "--max_depth" ]]; then
-    if [[ $# -ne 4 ]]; then
-        echo "Usage: $0 [--max_depth <depth>] <input_dir> <output_dir>"
+if [ "$1" = "--max_depth" ]; then
+    if [ $# -ne 4 ]; then
+        echo "Использование: $0 [--max_depth N] ВХОДНАЯ_ДИРЕКТОРИЯ ВЫХОДНАЯ_ДИРЕКТОРИЯ"
         exit 1
     fi
     max_depth="$2"
     input_dir="$3"
     output_dir="$4"
 else
-    if [[ $# -ne 2 ]]; then
-        echo "Usage: $0 [--max_depth <depth>] <input_dir> <output_dir>"
+    if [ $# -ne 2 ]; then
+        echo "Использование: $0 [--max_depth N] ВХОДНАЯ_ДИРЕКТОРИЯ ВЫХОДНАЯ_ДИРЕКТОРИЯ"
         exit 1
     fi
     input_dir="$1"
     output_dir="$2"
 fi
 
-if [[ ! -d "$input_dir" ]]; then
-    echo "Input directory does not exist: $input_dir"
+
+if [ ! -d "$input_dir" ]; then
+    echo "Входная директория не существует: $input_dir"
     exit 1
 fi
 
-
 mkdir -p "$output_dir"
 
-
-find_args=("$input_dir" -type f)
-if [[ $max_depth -gt 0 ]]; then
-    find_args+=(-maxdepth "$max_depth")
+if [ -n "$max_depth" ]; then
+    if ! [[ "$max_depth" =~ ^[0-9]+$ ]]; then
+        echo "Ошибка: --max_depth должен быть положительным целым числом."
+        exit 1
+    fi
+    find_maxdepth=$((max_depth + 1))
+else
+    find_maxdepth=""
 fi
 
+while IFS= read -r -d '' src_path; do
+    filename=$(basename "$src_path")
+    name="${filename%.*}"
+    ext="${filename##*.}"
 
-find "${find_args[@]}" -print0 | while IFS= read -r -d '' file; do
-    base_name=$(basename -- "$file")
-    dest="$output_dir/$base_name"
-    name_part="${base_name%.*}"
-    extension_part="${base_name##*.}"
-
-    # Обработка файлов без расширения
-    if [[ "$name_part" == "$extension_part" ]]; then
-        extension_part=""
-    else
-        extension_part=".$extension_part"
+  
+    if [ "$name" = "$filename" ]; then
+        ext=""
     fi
 
-   
+    dest_name="$filename"
     counter=1
-    while [[ -e "$dest" ]]; do
-        if [[ -n "$extension_part" ]]; then
-            dest="$output_dir/${name_part}_${counter}.${extension_part#.}"
+
+   
+    while [ -e "$output_dir/$dest_name" ]; do
+        if [ -z "$ext" ]; then
+            dest_name="${name}_${counter}"
         else
-            dest="$output_dir/${name_part}_${counter}"
+            dest_name="${name}_${counter}.${ext}"
         fi
-        ((counter++))
+        counter=$((counter + 1))
     done
 
-    cp -- "$file" "$dest"
-done
 
-exit 0
+    cp "$src_path" "$output_dir/$dest_name"
+done < <(
+    if [ -n "$find_maxdepth" ]; then
+        find "$input_dir" -type f -maxdepth "$find_maxdepth" -print0
+    else
+        find "$input_dir" -type f -print0
+    fi
+)
+
+echo "Файлы успешно скопированы в $output_dir"
