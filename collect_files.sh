cfb9c05ebad75a016d0c0d15a446721ffdc03411
kl1ment <122699425+Saltytie@users.noreply.github.com>
2025-04-29 00:18:19 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,52 +1,31 @@
 #!/bin/bash
 chmod +x collect_files.sh
 
-if [ $# -ne 2 ]; then
-    echo "Использование: $0 ВХОДНАЯ_ДИРЕКТОРИЯ ВЫХОДНАЯ_ДИРЕКТОРИЯ"
-    exit 1
-fi
 
-input_dir="$1"
-output_dir="$2"
+inputdir="$1"
+outputdir="$2"
 
+mkdir -p "$outputdir"
 
-if [ ! -d "$input_dir" ]; then
-    echo "Ошибка: Входная директория не существует: $input_dir"
-    exit 1
-fi
-
-
-mkdir -p "$output_dir"
-
-
-find "$input_dir" -type f -print0 | while IFS= read -r -d '' file; do
- 
-    filename=$(basename -- "$file")
-    
-    
-    name="${filename%.*}"
+find "$inputdir" -type f -print0 | while IFS= read -r -d '' file; do
+    filename="$(basename "$file")"
+    base="${filename%.*}"
     ext="${filename##*.}"
-    
-    
-    if [ "$name" = "$filename" ]; then
-        ext=""
-    fi
-    
-    dest_name="$filename"
-    counter=1
-    
 
-    while [ -e "$output_dir/$dest_name" ]; do
-        if [ -z "$ext" ]; then
-            dest_name="${name}_${counter}"
-        else
-            dest_name="${name}_${counter}.${ext}"
-        fi
-        ((counter++))
+    if [[ "$base" == "$filename" ]]; then
+        ext=""
+    else
+        ext=".$ext"
+    fi
+
+    dest="$filename"
+    index=1
+
+    while [[ -e "$outputdir/$dest" ]]; do
+        dest="${base}${index}${ext}"
+        ((index++))
     done
-    
-   
-    cp -- "$file" "$output_dir/$dest_name"
+
+    cp "$file" "$outputdir/$dest"
 done
 
-echo "Файлы успешно скопированы в $output_dir"
